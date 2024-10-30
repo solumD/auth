25ea5abe6131ecba2a06fada92e48d8077fdc32c
Dmitriy <dimkkononov@gmail.com>
2024-10-30 17:35:17 +0300

    add tests for api layer

---
+++ internal/api/user/tests/delete_user_test.go
@@ -0,0 +1,93 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/gojuno/minimock/v3"
+	"github.com/solumD/auth/internal/api/user"
+	"github.com/solumD/auth/internal/service"
+	serviceMocks "github.com/solumD/auth/internal/service/mocks"
+	desc "github.com/solumD/auth/pkg/auth_v1"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestDeleteUser(t *testing.T) {
+	t.Parallel()
+	type authServiceMockFunc func(mn *minimock.Controller) service.AuthService
+
+	type args struct {
+		ctx context.Context
+		req *desc.DeleteUserRequest
+	}
+
+	var (
+		ctx = context.Background()
+		mc  = minimock.NewController(t)
+
+		id = gofakeit.Int64()
+
+		serviceErr = fmt.Errorf("service error")
+
+		req = &desc.DeleteUserRequest{
+			Id: id,
+		}
+
+		res = &emptypb.Empty{}
+	)
+	defer t.Cleanup(mc.Finish)
+
+	tests := []struct {
+		name            string
+		args            args
+		want            *emptypb.Empty
+		err             error
+		authServiceMock authServiceMockFunc
+	}{
+		{
+			name: "success case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: res,
+			err:  nil,
+			authServiceMock: func(mc *minimock.Controller) service.AuthService {
+				mock := serviceMocks.NewAuthServiceMock(mc)
+				mock.DeleteUserMock.Expect(ctx, id).Return(&emptypb.Empty{}, nil)
+				return mock
+			},
+		},
+		{
+			name: "error case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: nil,
+			err:  serviceErr,
+			authServiceMock: func(mc *minimock.Controller) service.AuthService {
+				mock := serviceMocks.NewAuthServiceMock(mc)
+				mock.DeleteUserMock.Expect(ctx, id).Return(nil, serviceErr)
+				return mock
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			authServiceMock := tt.authServiceMock(mc)
+			api := user.NewImplementation(authServiceMock)
+
+			res, err := api.DeleteUser(tt.args.ctx, tt.args.req)
+			require.Equal(t, tt.err, err)
+			require.Equal(t, tt.want, res)
+		})
+	}
+}
