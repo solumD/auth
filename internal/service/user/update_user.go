21ee8cbc5f1ff71785168689e7f360a481160a98
Dmitriy <dimkkononov@gmail.com>
2024-10-31 16:49:24 +0300

    small fix

--- internal/service/user/update_user.go
+++ internal/service/user/update_user.go
@@ -13,17 +13,21 @@ import (
 // UpdateUser отправляет запрос в кэш, а затем в репо слой на обновление данных пользователя
 // UpdateUser отправляет запрос в кэш, а затем в репо слой на обновление данных пользователя
 func (s *srv) UpdateUser(ctx context.Context, user *model.UserUpdate) (*emptypb.Empty, error) {
-	err := validation.ValidateName(*user.Name)
-	if err != nil {
-		return nil, err
+	if user.Name != nil {
+		err := validation.ValidateName(*user.Name)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	err = validation.ValidateEmail(*user.Email)
-	if err != nil {
-		return nil, err
+	if user.Email != nil {
+		err := validation.ValidateEmail(*user.Email)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	_, err = s.authRepository.UpdateUser(ctx, user)
+	_, err := s.authRepository.UpdateUser(ctx, user)
 	if err != nil {
 		return nil, err
 	}
