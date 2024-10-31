c51b39112a8301707ec66605cddad7a3c0771852
Dmitriy <dimkkononov@gmail.com>
2024-10-31 11:21:18 +0300

    add validation

--- internal/service/user/create_user.go
+++ internal/service/user/create_user.go
@@ -2,15 +2,36 @@ package user
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/solumD/auth/internal/model"
+	"github.com/solumD/auth/internal/validation"
 )
 
 // CreateUser отправляет запрос в репо слой на создание пользователя
 func (s *srv) CreateUser(ctx context.Context, user *model.User) (int64, error) {
+	err := validation.ValidateName(user.Name)
+	if err != nil {
+		return 0, err
+	}
+
+	err = validation.ValidateEmail(user.Email)
+	if err != nil {
+		return 0, err
+	}
+
+	err = validation.ValidatePassword(user.Password)
+	if err != nil {
+		return 0, err
+	}
+
+	if user.Password != user.PasswordConfirm {
+		return 0, fmt.Errorf("password and passwordConfirm do not match")
+	}
+
 	var userID int64
 
-	err := s.txManager.ReadCommitted(ctx, func(ctx context.Context) error {
+	err = s.txManager.ReadCommitted(ctx, func(ctx context.Context) error {
 		var errTx error
 		userID, errTx = s.authRepository.CreateUser(ctx, user)
 		if errTx != nil {
--- internal/service/user/update_user.go
+++ internal/service/user/update_user.go
@@ -4,13 +4,24 @@ import (
 	"context"
 
 	"github.com/solumD/auth/internal/model"
+	"github.com/solumD/auth/internal/validation"
 
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // UpdateUser отправляет запрос в репо слой на обновление данных пользователя
 func (s *srv) UpdateUser(ctx context.Context, user *model.UserUpdate) (*emptypb.Empty, error) {
-	_, err := s.authRepository.UpdateUser(ctx, user)
+	err := validation.ValidateName(*user.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	err = validation.ValidateEmail(*user.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = s.authRepository.UpdateUser(ctx, user)
 	if err != nil {
 		return nil, err
 	}
